32b034f3757e4e4cce58217b107beb9177b8b930
Matthew Robinson <110871707+Mattrob10@users.noreply.github.com>
2022-10-04 11:37:30 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+const generateNumber = () => {
+  // Generate # between 1-10
+  const randomNumber = Math.floor(Math.random() * 100 + 1); 
+  number.innerHTML = randomNumber;
+}
+
+btn.addEventListener('click', generateNumber);
+
+generateNumber();
+
+
+
+
+
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
